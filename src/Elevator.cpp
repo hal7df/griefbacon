99f49e06dc7961d85170e8adf21618beb4aaefbf
Paul  Bunyan <hal7df@gmail.com>
2015-02-14 14:23:04 -0500

    (Hopefully) Finished PID profiling.
    Still have yet to find the full time value for scaling.

--- src/Elevator.cpp
+++ src/Elevator.cpp
@@ -14,12 +14,18 @@ Elevator::Elevator(Victor* lElevator, Victor* rElevator, Relay* binExt, Encoder*
 	m_rElevator = rElevator;
 	m_binExt = binExt;
 	m_elevEncode = encode;
+	m_elevEncode->SetDistancePerPulse(1./1270.);
 
 	m_pid = new PIDController(ELEVATOR_P, ELEVATOR_I, ELEVATOR_D, m_elevEncode, this);
 
-	m_elevEncode->SetDistancePerPulse(1./1270.);
+	m_timer = new Timer;
+	m_profileCase = 0;
 
-	f_getPID = false;
+	f_setPID = false;
+	f_oldPID = false;
+	f_pidRunning = false;
+
+	m_setpoint = 0.0;
 }
 
 Elevator::Elevator(int lElevator, int rElevator, int binExt, int encode)
@@ -29,12 +35,18 @@ Elevator::Elevator(int lElevator, int rElevator, int binExt, int encode)
 	m_rElevator = new Victor (rElevator);
 	m_binExt = new Relay (binExt);
 	m_elevEncode = new Encoder (encode,encode++,true);
+	m_elevEncode->SetDistancePerPulse(1./1270.);
 
 	m_pid = new PIDController (ELEVATOR_P, ELEVATOR_I, ELEVATOR_D, m_elevEncode, this);
 
-	m_elevEncode->SetDistancePerPulse(1./1270.);
+	m_timer = new Timer;
+	m_profileCase = 0;
 
-	f_getPID = false;
+	f_setPID = false;
+	f_oldPID = false;
+	f_pidRunning = false;
+
+	m_setpoint = 0.0;
 }
 
 Elevator::~Elevator() {
@@ -54,36 +66,71 @@ void Elevator::Set (Relay::Value direction)
 
 void Elevator::Set (pos_t position)
 {
-	switch (position){
-	case kBottom:
-		m_pid->SetSetpoint(ELEVATOR_BOTTOM);
-		break;
-	case kTop:
-		m_pid->SetSetpoint(ELEVATOR_TOP);
-		break;
-	case kLMid:
-		m_pid->SetSetpoint(ELEVATOR_LMID);
-		break;
-	case kUMid:
-		m_pid->SetSetpoint(ELEVATOR_UMID);
-		break;
-	case kCarry:
-		m_pid ->SetSetpoint(ELEVATOR_CARRY);
-		break;
-	}
+	if (f_oldPID)
+		SetSetpoint_old(position);
+	else
+		SetSetpoint(position);
+}
 
-	if (!m_pid->IsEnabled())
+void Elevator::Enable ()
+{
+	if (f_oldPID)
 		m_pid->Enable();
+	else
+	{
+		FindCoefficients();
+		f_pidRunning = true;
+		m_profileCase = 0;
+		m_timer->Stop();
+		m_timer->Start();
+		m_timer->Reset();
+	}
+}
+
+void Elevator::Disable()
+{
+	if (f_oldPID && m_pid->IsEnabled())
+		m_pid->Disable();
+	else if (!f_oldPID && m_pid->IsEnabled())
+	{
+		f_pidRunning = false;
+		m_pid->Disable();
+	}
+}
+
+bool Elevator::IsEnabled()
+{
+	return (!f_oldPID && f_pidRunning) || m_pid->IsEnabled();
 }
 
 void Elevator::Update ()
 {
+	if (!f_oldPID && f_pidRunning)
+	{
+		switch (m_profileCase)
+		{
+		case 0:
+			m_pid->SetSetpoint(GetProfilePosition(m_timer->Get()));
 
+			if (!m_pid->IsEnabled())
+				m_pid->Enable();
+
+			if (m_timer->HasPeriodPassed(GetProfileTime(m_lastSetpoint,m_setpoint)))
+				m_profileCase++;
+
+			break;
+		case 1:
+			m_timer->Stop();
+			m_timer->Reset();
+			m_pid->SetSetpoint(m_setpoint);
+			m_profileCase++;
+		}
+	}
 }
 
 void Elevator::PrintData()
 {
-	if (f_getPID)
+	if (f_setPID)
 		m_pid->SetPID(SmartDashboard::GetNumber("Elevator P"),SmartDashboard::GetNumber("Elevator I"),SmartDashboard::GetNumber("Elevator D"));
 	else
 	{
@@ -95,4 +142,101 @@ void Elevator::PrintData()
 		SmartDashboard::PutNumber("Elevator D",m_pid->GetD());
 		SmartDashboard::PutNumber("Elevator PID Output",m_pid->Get());
 	}
+}
+
+/** PRIVATE **/
+
+void Elevator::SetSetpoint(pos_t position)
+{
+	m_lastSetpoint = m_elevEncode->GetDistance();
+	m_setpoint = GetPosition(position);
+	Enable();
+}
+
+void Elevator::SetSetpoint_old(pos_t position)
+{
+	switch (position){
+		case kBottom:
+			m_pid->SetSetpoint(ELEVATOR_BOTTOM);
+			break;
+		case kTop:
+			m_pid->SetSetpoint(ELEVATOR_TOP);
+			break;
+		case kLMid:
+			m_pid->SetSetpoint(ELEVATOR_LMID);
+			break;
+		case kUMid:
+			m_pid->SetSetpoint(ELEVATOR_UMID);
+			break;
+		case kCarry:
+			m_pid ->SetSetpoint(ELEVATOR_CARRY);
+			break;
+		}
+
+	if (!m_pid->IsEnabled())
+		m_pid->Enable();
+}
+
+void Elevator::FindCoefficients ()
+{
+	//3rd-order coefficient
+	m_coeff.a3 = ((1/(2*pow(GetProfileTime(m_lastSetpoint,m_setpoint),3)))*20*GetDelta(m_lastSetpoint,m_setpoint));
+
+	//4th-order coefficient
+	m_coeff.a4 = ((1/(2*pow(GetProfileTime(m_lastSetpoint,m_setpoint),4)))*30*GetReverseDelta(m_lastSetpoint,m_setpoint));
+
+	//5th-order coefficient
+	m_coeff.a5 = ((1/(2*pow(GetProfileTime(m_lastSetpoint,m_setpoint),5)))*12*GetDelta(m_lastSetpoint,m_setpoint));
+}
+
+float Elevator::GetProfilePosition (double time)
+{
+	double term3, term4, term5;
+
+	term3 = m_coeff.a3*(pow(time,3));
+	term4 = m_coeff.a4*(pow(time,4));
+	term5 = m_coeff.a5*(pow(time,5));
+
+	return (float)(term5 + term4 + term3 + m_lastSetpoint);
+}
+
+double Elevator::GetProfileTime(double start, double end)
+{
+	return (ELEVATOR_TIME_FULL*GetDelta(start,end));
+}
+
+double Elevator::GetDelta(double start, double end)
+{
+	return end - start;
+}
+
+double Elevator::GetReverseDelta(double start, double end)
+{
+	return start - end;
+}
+
+double Elevator::GetPosition (pos_t position)
+{
+	double pos;
+
+	switch (position)
+	{
+	case kBottom:
+		pos = ELEVATOR_BOTTOM;
+		break;
+	case kTop:
+		pos = ELEVATOR_TOP;
+		break;
+	case kLMid:
+		pos = ELEVATOR_LMID;
+		break;
+	case kUMid:
+		pos = ELEVATOR_UMID;
+		break;
+	case kCarry:
+		pos = ELEVATOR_CARRY;
+		break;
+	}
+
+	return pos;
 }
--- src/Elevator.h
+++ src/Elevator.h
@@ -11,16 +11,22 @@
 #include "RobotUtils/HotSubsystem.h"
 #include "RobotUtils/RobotUtils.h"
 #include "WPILib.h"
+#include <cmath>
 
+//PID gains
 #define ELEVATOR_P 8.0
 #define ELEVATOR_I 0.01
 #define ELEVATOR_D 0.0
 
+//PID setpoints
 #define ELEVATOR_BOTTOM 0.0
 #define ELEVATOR_TOP 1.0
 #define ELEVATOR_LMID 0.43
 #define ELEVATOR_UMID 0.76
 #define ELEVATOR_CARRY 0.177
+
+//Profile times
+#define ELEVATOR_TIME_FULL 0.0
 
 enum pos_t {
 	kBottom,
@@ -28,6 +34,12 @@ enum pos_t {
 	kLMid,
 	kUMid,
 	kCarry
+};
+
+struct co_t {
+	double a3;
+	double a4;
+	double a5;
 };
 
 class Elevator: public HotSubsystem, public PIDOutput {
@@ -43,25 +55,47 @@ public:
 	void Set (Relay::Value direction);
 	void Set (pos_t position);
 
-	void Enable () { m_pid->Enable(); }
-	void Disable () { if (IsEnabled()) m_pid->Disable(); }
-	bool IsEnabled() { return m_pid->IsEnabled(); }
+	void Enable ();
+	void Disable ();
+	bool IsEnabled();
+	bool AtPosition () { return m_profileCase > 0; }
 	void Reset () { m_elevEncode->Reset(); }
 
 	void PIDWrite (float input) { Set((double)input); }
 
-	void GetPID (bool get) { f_getPID = get; }
+	void SetPID (bool set) { f_setPID = set; }
+	void SetOldPID (bool old) { f_oldPID = old; Disable(); }
 protected:
 	void Update();
 	void PrintData();
 private:
+	void SetSetpoint_old (pos_t position);
+	void SetSetpoint (pos_t position);
+
+	void FindCoefficients ();
+	float GetProfilePosition (double time);
+
+	double GetProfileTime (double start, double end);
+	double GetDelta (double start, double end);
+	double GetReverseDelta (double start, double end);
+	double GetPosition (pos_t position);
+
 	Victor* m_lElevator;
 	Victor* m_rElevator;
 	Relay* m_binExt;
 	Encoder* m_elevEncode;
 	PIDController* m_pid;
+	Timer* m_timer;
 
-	bool f_getPID;
+	bool f_setPID;
+	bool f_oldPID;
+	bool f_pidRunning;
+
+	unsigned m_profileCase;
+
+	double m_setpoint;
+	double m_lastSetpoint;
+	co_t m_coeff;
 };
 
 #endif /* SRC_ELEVATOR_H_ */
