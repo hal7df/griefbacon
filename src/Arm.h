41c11595cc7da694d886e06ac582f157eb16e328
Paul  Bunyan <hal7df@gmail.com>
2015-04-08 15:11:57 -0400

    Started work on the can burglar.
    - New auton enum case for CanBurglar Auton.
    - New PID wrapper for the can burglar system.
    - Base can burglar code written.

--- src/Arm.h
+++ src/Arm.h
@@ -10,7 +10,9 @@
 
 #include "RobotUtils/HotSubsystem.h"
 #include "WPILib.h"
+#include "BurgleWrapper.h"
 #include <semaphore.h>
+#include <cmath>
 
 #define PRACTICE_BOT
 //#define COMPETITION_BOT
@@ -29,6 +31,10 @@
 #define SHOULDER_P -8.0
 #define SHOULDER_I -0.01
 #define SHOULDER_D 0.0
+
+#define BURGLE_P 0.1
+#define BURGLE_I 0.0
+#define BURGLE_D 0.0
 
 #define SHOULDER_GROUND -0.892 //-0.931
 #define SHOULDER_TWOTOTE -0.831
@@ -83,7 +89,7 @@ enum wPos_t {
 class Arm: public HotSubsystem, public PIDOutput {
 public:
 	friend class HotSubsystemHandler;
-	Arm(int pickSL, int pickSR, int pickW, int pickRL, int pickRR, int intakeL, int intakeR);
+	Arm(int pickSL, int pickSR, int pickW, int pickRL, int pickRR, int intakeL, int intakeR, int canburgleL, int canburgleR, int potL, int potR);
 	virtual ~Arm();
 
 	void shoulderSet(double speed);
@@ -95,6 +101,9 @@ public:
 	void intakeSet(double speed);
 
 	void clearCans (bool on);
+
+	void setBurgle (bool on) { f_burgling = on;}
+	bool burglarAtPoint (float point) { return fabs(((m_potL->Get() + m_potR->Get())/2)-point) < 0.1; }
 
 	bool WristAtSetpoint ();
 	bool ShoulderAtSetpoint ();
@@ -141,21 +150,35 @@ private:
 	CANTalon* m_intakeL;
 	CANTalon* m_intakeR;
 
+	Talon* m_canburgleL;
+	Talon* m_canburgleR;
+
 	Encoder* m_shoulderEncode;
 	Encoder* m_wristEncode;
 
+	AnalogPotentiometer* m_potL;
+	AnalogPotentiometer* m_potR;
+
+	BurgleWrapper* m_burgleWrap;
+
 	PIDController* m_shoulderPid;
 	PIDController* m_wristPid;
-	bool f_getPID;
+	PIDController* m_burglePid;
 
+	unsigned m_burgleCase;
+
+	bool f_burgling;
+	bool f_getPID;
 	bool f_eStopRunning;
 	bool f_wSetpointChanged;
 	bool f_sSetpointChanged;
 	bool f_wEStop;
 	bool f_sEStop;
 	bool f_shoulderStop;
+
 	Timer* m_wStopTime;
 	Timer* m_sStopTime;
+	Timer* m_burgletime;
 	sem_t m_semaphore;
 };
 
--- src/griefbacon.cpp
+++ src/griefbacon.cpp
@@ -15,7 +15,8 @@ enum auton_t {
 	kTwoCan,
 	kDriveForward,
 	kKnockCanGoAutoZone,
-	kNothing
+	kNothing,
+	kCanBurglar
 };
 
 
@@ -54,7 +55,7 @@ public:
 		m_operator->SetDeadbandType(AdvancedJoystick::kQuad);
 
 		m_drivetrain = new Drivetrain (0,2,0,2);
-		m_arm = new Arm(11,16,14,10,15,12,13);
+		m_arm = new Arm(11,16,14,10,15,12,13,1,3,0,1);
 		m_elev = new Elevator (4,5,0,8);
 		m_debug = new BackgroundDebugger (1000,true);
 
@@ -138,7 +139,7 @@ public:
 
 
 		if (m_operator->GetRawButton(AdvancedJoystick::kButtonB))
-			m_autonChoice = kTwoCan;
+			m_autonChoice = kCanBurglar;
 		else if (m_operator->GetRawButton(AdvancedJoystick::kButtonX))
 			m_autonChoice = kKnockCanGoAutoZone;
 		else if (m_operator->GetRawButton(AdvancedJoystick::kButtonY))
@@ -161,8 +162,8 @@ public:
 		case kThreeTotew90Turn:
 			SmartDashboard::PutString("DB/String5","Three Tote w 90 Turn");
 			break;
-		case kTwoCan:
-			SmartDashboard::PutString("DB/String5","Two Can Clear");
+		case kCanBurglar:
+			SmartDashboard::PutString("DB/String5","Can Burglar");
 			break;
 		case kDriveForward:
 			SmartDashboard::PutString("DB/String5", "Drive Forward");
